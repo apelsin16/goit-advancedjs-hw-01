3d28f27cdb0ff1e079fdfb5c0174e234497c8222
Vitalii Kostenko <apelsin@i.ua>
2024-09-26 17:45:35 +0200

    ready

---
+++ src/js/2-form.js
@@ -0,0 +1,31 @@
+const formData = {
+    email: '',
+    message: ''
+}
+
+const form = document.querySelector(".feedback-form");
+
+const localStorageKey = "feedback-form-state";
+
+if(localStorage.getItem(localStorageKey)) {
+    const parsedData = JSON.parse(localStorage.getItem(localStorageKey));
+    
+    formData.email = parsedData.email;
+    formData.message = parsedData.message;
+    form.elements.email.value = parsedData.email;
+    form.elements.message.value = parsedData.message;
+}
+
+form.addEventListener("input", (evt) => {
+
+    let name = evt.target.name;
+
+    formData[name] = form.elements[name].value;
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
+
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+});
